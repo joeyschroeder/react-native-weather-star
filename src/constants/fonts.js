d5440536ddb2046c1cb2a81c15310561e8b1e674
Joey Schroeder <schrjos@gmail.com>
2024-02-27 09:50:47 -0600

    Add fonts constant

---
+++ src/constants/fonts.js
@@ -0,0 +1,34 @@
+export const FONTS = {
+  SANS_SERIF: {
+    BOLD: 'helvetica-neue-bold',
+    BOLD_ITALIC: 'helvetica-neue-bold-italic',
+    LIGHT: 'helvetica-neue-light',
+    LIGHT_ITALIC: 'helvetica-neue-light-italic',
+    REGULAR: 'helvetica-neue-regular',
+    REGULAR_ITALIC: 'helvetica-neue-regular-italic',
+    ULTRA_LIGHT: 'helvetica-neue-ultra-light',
+    ULTRA_LIGHT_ITALIC: 'helvetica-neue-ultra-light-italic',
+  },
+  SCRIPT: {
+    BOLD: 'syncopate-bold',
+    REGULAR: 'syncopate-regular',
+  },
+  MONO: {
+    LETTER: {
+      BOLD: 'dseg-14-classic-bold',
+      BOLD_ITALIC: 'dseg-14-classic-bold-italic',
+      ITALIC: 'dseg-14-classic-italic',
+      LIGHT: 'dseg-14-classic-light',
+      LIGHT_ITALIC: 'dseg-14-classic-light-italic',
+      REGULAR: 'dseg-14-classic-regular',
+    },
+    NUMBER: {
+      BOLD: 'dseg-7-classic-bold',
+      BOLD_ITALIC: 'dseg-7-classic-bold-italic',
+      ITALIC: 'dseg-7-classic-italic',
+      LIGHT: 'dseg-7-classic-light',
+      LIGHT_ITALIC: 'dseg-7-classic-light-italic',
+      REGULAR: 'dseg-7-classic-regular',
+    },
+  },
+};
