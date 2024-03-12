725cd6ec61b281d9c8470584e2f8bca5e1c68c24
Joey Schroeder <schrjos@gmail.com>
2024-03-12 08:11:06 -0500

    feat: Add AsyncStorage service

---
+++ src/services/async-storage/async-storage.js
@@ -0,0 +1,16 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const getAsyncStorageItem = async (key) => {
+  if (!key) throw new Error('Key is undefined.');
+
+  const response = await AsyncStorage.getItem(key);
+  if (!response) return undefined;
+  return JSON.parse(response);
+};
+
+export const setAsyncStorageItem = (key = '', value = '') => {
+  if (!key) throw new Error('Key is undefined.');
+  if (!value) throw new Error('Value is undefined.');
+
+  return AsyncStorage.setItem(key, JSON.stringify(value));
+};
